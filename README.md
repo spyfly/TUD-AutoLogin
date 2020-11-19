5dcaaa2692913363dc06f686b38b1a7f1d362932
Sebastian Heiden <seb.heiden@gmail.com>
2020-11-19 20:49:49 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# OPAL AutoLogin
+
+This is a browser script, that performs an automatic login for the following TU Dresden web services:
+- [OPAL](https://bildungsportal.sachsen.de/opal)
+- and any other service with the TUD Login Page
+
+## Installation
+1. Download the [Tampermonkey Extension](https://www.tampermonkey.net) for your Browser
+2. Install the script by [clicking here](https://raw.githubusercontent.com/spyfly/TUD-AutoLogin/script.js)
+3. (Optional) Add your TUD Credentials to the configuration in the script file
+
+## Usage
+Visit your favorite TUD websites and enjoy automatic login.
+
+## Future development and issues
+If you have improvement ideas or encounter issues with the script don't hesitate to open a [new issue](https://github.com/spyfly/TUD-AutoLogin/issues). This could be a request for adding more supported services or reporting a bug.
---
+++ script.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         TUD AutoLogin
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Stop wasting your time entering login credentials or pressing useless buttons!
+// @source       spyfly
+// @website      https://github.com/spyfly/TUD-AutoLogin
+// @match        https://bildungsportal.sachsen.de/*
+// @match        https://idp2.tu-dresden.de/*
+// @updateURL    https://raw.githubusercontent.com/spyfly/TUD-AutoLogin/script.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Enter your login creds here
+    const tud = {
+      username: "",
+      password: ""
+    }
+
+    // Code starts here
+    var isOpalLoginPage = (document.getElementsByName("shibLogin").length >= 1);
+    var isTudLoginPage = (window.location.host == "idp2.tu-dresden.de")
+
+    if (isOpalLoginPage) {
+      //Select TUD if it hasn't been selected yet
+      if (document.getElementsByName("wayfselection")[0].selectedIndex == 0) {
+        document.getElementsByName("wayfselection")[0].selectedIndex = 19;
+      }
+
+      //Press Login Button on OPAL Page
+      document.getElementsByName("shibLogin")[0].click();
+    } else if (isTudLoginPage) {
+      // We are on the TUD I2DP Page
+      var hasLoginField = (document.getElementById("username") != undefined);
+
+      if (hasLoginField) {
+        // Try to fill in credentials
+        document.getElementById("username").value = tud.username;
+        document.getElementById("password").value = tud.password;
+        if (tud.username.length > 0 && tud.password.length > 0) {
+          document.getElementsByName("_eventId_proceed")[0].click();
+        }
+      } else {
+        // Just press the continue button
+        document.getElementsByName("_eventId_proceed")[0].click();
+      }
+    }
+})();
